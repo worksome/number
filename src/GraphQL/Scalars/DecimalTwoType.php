39e057a664dfa8a4dbf9521f8e1ad614a0494de9
Owen Voke <development@voke.dev>
2025-02-14 09:53:02 +0000

    feat: add new DecimalTwo scalar

---
+++ src/GraphQL/Scalars/DecimalTwoType.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Worksome\Number\GraphQL\Scalars;
+
+use Brick\Math\Exception\NumberFormatException;
+use GraphQL\Error\Error;
+use GraphQL\Language\AST\FloatValueNode;
+use GraphQL\Language\AST\IntValueNode;
+use GraphQL\Language\AST\Node;
+use GraphQL\Type\Definition\ScalarType;
+use Worksome\Number\Exceptions\NumberException;
+use Worksome\Number\Number;
+
+final class DecimalTwoType extends ScalarType
+{
+    public string|null $description = <<<TXT
+        The `DecimalTwo` scalar type represents a number with 2 decimal places.
+        TXT;
+
+    /**
+     * @param string|int|float $value
+     *
+     * @return float
+     *
+     * @throws Error
+     */
+    public function serialize($value)
+    {
+        return $this->parseValue($value)->toFloat();
+    }
+
+    /**
+     * @param string|int|float $value
+     *
+     * @return Number
+     *
+     * @throws Error
+     */
+    public function parseValue($value)
+    {
+        try {
+            return Number::of($value);
+        } catch (NumberException|NumberFormatException $exception) {
+            throw new Error($exception->getMessage());
+        }
+    }
+
+    public function parseLiteral(Node $valueNode, array|null $variables = null)
+    {
+        if (! $valueNode instanceof IntValueNode && ! $valueNode instanceof FloatValueNode) {
+            throw new Error('Query error: Can only parse integer or float. Got: ' . $valueNode->kind, [$valueNode]);
+        }
+
+        return Number::of($valueNode->value);
+    }
+}
