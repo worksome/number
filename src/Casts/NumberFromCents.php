2bac9d096d806f65788f6ee8c23194324a022e92
Owen Voke <development@voke.dev>
2025-03-13 10:15:40 +0000

    feat: add support for Laravel 12

--- src/Casts/NumberFromCents.php
+++ src/Casts/NumberFromCents.php
@@ -8,9 +8,7 @@ use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Worksome\Number\Exceptions\ValueIsNotANumberException;
 use Worksome\Number\Number;
 
-/**
- * @implements CastsAttributes<Number, Number>
- */
+/** @implements CastsAttributes<Number, Number> */
 class NumberFromCents implements CastsAttributes
 {
     /** @param int $value */
@@ -22,6 +20,7 @@ class NumberFromCents implements CastsAttributes
     /** @param Number $value */
     public function set($model, string $key, $value, array $attributes)
     {
+        // @phpstan-ignore instanceof.alwaysTrue (we type hint `Number` as it should be one, but there's still a possibility that it isn't.)
         if (! $value instanceof Number) {
             throw ValueIsNotANumberException::fromCents();
         }
