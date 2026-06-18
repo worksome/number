308723f0577a636900ab89bb41d74d07cd5aa0e5
Owen Voke <development@voke.dev>
2026-06-18 10:14:32 +0100

    chore(deps-dev): Update to Coding Style 4.x
    This updates to the latest Coding Style.

--- ecs.php
+++ ecs.php
@@ -2,15 +2,11 @@
 
 declare(strict_types=1);
 
-use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Worksome\CodingStyle\WorksomeEcsConfig;
 
-
-return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->paths([
+return WorksomeEcsConfig::configure()
+    ->withPaths([
+        __DIR__ . '/ecs.php',
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
-
-    WorksomeEcsConfig::setup($ecsConfig);
-};
